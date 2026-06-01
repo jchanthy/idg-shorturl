632d76801540e0e1a06c1a472ef2a7e212da316d
John Chanthy <john.chanthy@gmail.com>
2026-06-01 12:53:15 +0700

    Debug: Add API key to debug2.js

--- api/debug2.js
+++ api/debug2.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   try {
     const projectId = process.env.VITE_FIREBASE_PROJECT_ID;
-    const url = `https://firestore.googleapis.com/v1/projects/${projectId}/databases/(default)/documents/links`;
+    const apiKey = process.env.VITE_FIREBASE_API_KEY;
+    const url = `https://firestore.googleapis.com/v1/projects/${projectId}/databases/(default)/documents/links?key=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
